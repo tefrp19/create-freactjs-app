f806168d1f0f26f3d2652c42c36b02a7707897d2
tefrp <1633467021@qq.com>
2023-03-20 22:29:16 +0800

    完成项目

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 模仿`Create-React-App`的`create-freactjs-app` node shell命令
+
+# 用法
+
+1. `npx create-freactjs-app my-app`
+2. `cd my-app`
+3. `npm run dev`
+
+## 打包与部署
+
+本项目使用`rollup`打包，开发时直接生成打包好的的存档文件（在public文件夹中），所以public文件夹可以直接部署。
+
+
+
+# 依赖说明
+
+包`freactjs-scripts` 其中包含新项目的template和rollup、babel的config
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,3 +1,82 @@
 #!/usr/bin/env node
 
-console.log('执行node shell')
+const path = require('path')
+const fs = require('fs-extra')
+const os = require('os')
+const spawn = require("cross-spawn")
+
+const projectName = process.argv[2]
+if (projectName === undefined) {
+    console.log('未指定项目名')
+    process.exit(1)
+}
+
+const root = path.resolve(projectName); // 项目绝对路径
+console.log('root:', root)
+
+fs.ensureDirSync(root) // 创建项目文件夹
+
+const packageJson = {
+    name: projectName,
+    version: '0.1.0',
+    type: "module",
+    scripts: {
+        "dev": "rollup -c -w"
+    }
+};
+fs.writeFileSync( // 写入package.json文件
+    path.join(root, 'package.json'),
+    JSON.stringify(packageJson, null, 2) + os.EOL
+);
+
+const originalDirectory = process.cwd();
+console.log(originalDirectory)
+
+process.chdir(root); // 切换node 进程的工作目录
+
+// 1.安装新项目所需的依赖性：rollup、babel等
+function install() {
+    const devDependencies = [
+        "freactjs",
+        '@babel/core',
+        "@babel/preset-env",
+        "@babel/preset-react",
+        "@rollup/plugin-babel",
+        "@rollup/plugin-node-resolve",
+        "rollup",
+        "rollup-plugin-import-css",
+        "rollup-plugin-livereload",
+        "rollup-plugin-serve"
+    ]
+    return new Promise((resolve, reject) => {
+        const child = spawn(
+            'npm',
+            ['install', '--save-dev', ...devDependencies],
+            {stdio: 'inherit'}
+        );
+
+        child.on('close', code => {
+            if (code !== 0) {
+                reject('失败');
+                return;
+            }
+            resolve();
+        });
+    })
+
+}
+
+install().then(() => {
+    const freactjsScriptsPath = path.dirname(
+        require.resolve('freactjs-scripts/package.json')
+    );
+    console.log(freactjsScriptsPath)
+    // 2.下载新项目基本模板
+    // 将template文件夹中的所有内容复制到新项目中
+    fs.copySync(path.join(freactjsScriptsPath, 'template'), root);
+
+    // 3.下载新项目配置文件（rollup、babel）
+    fs.copySync(path.join(freactjsScriptsPath, 'config'), root);
+
+})
+
